2992332b520b6631a8f5428095a37cc5e84597b6
161250102 <161250102@smail.nju.edu.cn>
2018-07-18 15:14:09 +0800

    增加登录需要的路由导航

--- src/router.js
+++ src/router.js
@@ -1,12 +1,31 @@
 import VueRouter from 'vue-router'
 import Login from './components/Login'
+import Index from './components/Index'
 
-export default new VueRouter({
+const router = new VueRouter({
+  mode: 'history',
   routes: [
     {
       path: '/login',
       name: 'Login',
-      component: Login
+      component: Login,
+      meta: {title: '登录 | 学生综合服务平台'},
+    },
+    {
+      path: '/main',
+      name: 'Main',
+      component: Index,
+      children: [
+
+      ]
     }
   ]
-})
+});
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+});
+
+export default router
