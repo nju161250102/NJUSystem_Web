a515d4a6630d020ab7126e31d728cfd824acb7c4
161250102 <161250102@smail.nju.edu.cn>
2018-07-18 13:35:12 +0800

    增加登录用的mock

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-footer style="background-color: #6A005F" :height="footerHeight">
+    <v-layout row wrap align-center>
+      <v-flex xs12 text-xs-center>
+        <div class="white--text ml-3">
+          Made By 161250102
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    name: "Footer",
+    props: ['footerHeight']
+  }
+</script>
+
+<style scoped>
+
+</style>
