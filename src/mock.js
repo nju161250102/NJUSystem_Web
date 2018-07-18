38af948dcced59cef77f8d1afb9749f27d2b4dbd
161250102 <161250102@smail.nju.edu.cn>
2018-07-18 13:34:55 +0800

    增加登录用的mock

--- src/mock.js
+++ src/mock.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const Mock =  require("mockjs");
-
 const router = express.Router();
+
+router.get('/personInfo', (req, res) => {
+  res.json({
+    name: Mock.Random.first(),
+    id: Mock.Random.natural(161250001, 161250222),
+  });
+});
+
+router.post('/login', (req, res) => {
+  res.json(['cas_success'])
+});
 
 module.exports = router;
